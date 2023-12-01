3fde5ce1032b52db84dcc06cc5e8b8d7252bf9a8
Jim Hult <jimhult@hotmail.se>
2023-12-01 16:05:23 +0100

    day1 part2 done

--- day1/main2.py
+++ day1/main2.py
@@ -1,18 +1,20 @@
 # Open the file
-# file_path = 'file.txt'  # Replace with your file path
-file_path = 'testfile2.txt'  # Replace with your file path
+file_path = 'file.txt'  # Replace with your file path
+# file_path = 'testfile2.txt'  # Replace with your file path
 
 def isDigit(input: int):
     for i in range(10):
         if i == input:
             return True
         
-def containsDigitInStr(input: str):
-    for strDigit in digitsInStr:
-        if strDigit in input:
-            return strDigit
+def containsDigitInStr(input: str) -> int | None:
+    for i, strDigit in enumerate(digitsInStr):
+        found_str_idx: int = input.find(strDigit)
+        if found_str_idx != -1:
+            return digit1to9[i]
 
 digitsInStr = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+digit1to9 = [1,2,3,4,5,6,7,8,9]
     
         
 content = ""
@@ -27,37 +29,45 @@ content = content.split()
 
 # first digits
 for word in content:
-    print("first word :?", word)
-
     concat_letters: str = ""
+
     for letter in word:
         concat_letters += letter
+
+        # check if str digit
+        str_digit: int | None = containsDigitInStr(concat_letters)
+        if str_digit != None and str_digit != -1:
+            first_digits += str(str_digit)
+            break
 
         # check if digit
         try:
             if isDigit(int(letter)):
                 first_digits += letter
-                print("first ok")
                 break
         except:
-            print("first wrong")
             pass
         
-        # check if digit in str
 
 
 # last digits
 for word in content:
+    concat_letters: str = ""
     for letter_index in range(len(word)-1, -1, -1):
+        concat_letters = word[letter_index] + concat_letters
+
+        # check if str digit
+        str_digit: int | None = containsDigitInStr(concat_letters)
+        if str_digit != None and str_digit != -1:
+            last_digits += str(str_digit)
+            break
         
         try:
             if isDigit(int(word[letter_index])):
                 last_digits += word[letter_index]
-                print("ok")
                 break
         except:
-            print("wrong")
-
+            pass
 
 print("first digits: ", first_digits)
 print("last digits: ", last_digits)
