6e1612f0fe69b089fcd4f836f7d033a958548d11
Jim Hult <jimhult@hotmail.se>
2023-12-01 07:26:36 +0100

    day1 part1 done

--- day1/main.py
+++ day1/main.py
@@ -1,10 +1,55 @@
 # Open the file
 file_path = 'file.txt'  # Replace with your file path
+# file_path = 'testfile.txt'  # Replace with your file path
+
+def isDigit(input: int):
+    for i in range(10):
+        if i == input:
+            return True
+        
+content = ""
 with open(file_path, 'r') as file:
     # Read the contents
     content = file.read()
 
-    # Print the contents
-    print(content)
+first_digits = ""
+last_digits = ""
 
-# The file is automatically closed after the with block
+content = content.split()
+
+for word in content:
+    # print("word :?", word)
+
+    # first digits
+    for letter in word:
+        try:
+            if isDigit(int(letter)):
+                first_digits += letter
+                # print("ok")
+                break
+        except:
+            # print("wrong")
+            pass
+
+print("test1: ")
+for word in content:
+    # last digits
+    for letter_index in range(len(word)-1, -1, -1):
+        
+        try:
+            if isDigit(int(word[letter_index])):
+                last_digits += word[letter_index]
+                print("ok")
+                break
+        except:
+            print("wrong")
+
+
+print("first digits: ", first_digits)
+print("last digits: ", last_digits)
+
+answer = 0
+for i in range(len(first_digits)):
+    answer += int(first_digits[i] + last_digits[i])
+    
+print("Solution: ", answer)
