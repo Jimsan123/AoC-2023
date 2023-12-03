aa7d51421020a8f675d6ef3f19e85f0da3a97110
Jim Hult <jimhult@hotmail.se>
2023-12-03 18:45:16 +0100

    part 1 done

--- day3/part1.py
+++ day3/part1.py
@@ -1,69 +1,72 @@
 import string
-file_path = "testfile.txt"
+file_path = "file.txt"
 
 schematics = ""
 with open(file_path, 'r') as file:
     schematics = file.readlines()
 
-# print(len(schematics))
-# def checkAbove(row_above: str, curr_idx):
-#     found_numbers = []
-#     if curr_idx == 0:
-#         if row_above[curr_idx] in "0123456789":
-#             searchWholeNumber()
-
-#     elif curr_idx == len(schematics):
-
-#     else:
-
-# def searchWholeNumber():
-#     pass
-
-
 # Find all symbols in the schematic
 symbol_indexes: list[(int, int)] = []
 for row_num, row in enumerate(schematics):
-    print(row_num, ": ", row)
+    # print(row_num, ": ", row)
 
     # Find the specific symbols
     for char_idx, character in enumerate(row):
-        if character in string.punctuation and character != ".":
+        if character in "#-*=+@&%$/" and character != ".":
             symbol_indexes.append((row_num, char_idx))
 
-
-print("all found symbols: \n", symbol_indexes)
+# print("all found symbols: \n", symbol_indexes)
 
 # Find all numbers
 numbers: list[(int, int, str)] = []
 for row_num, row in enumerate(schematics):
     current_number: str = ""
     for char_idx, character in enumerate(row):
-        
+
         if character in "0123456789":
             current_number += character
         elif current_number != "":
             numbers.append((row_num, char_idx-1, current_number))
             current_number = ""
 
-print("All found numbers \n", numbers)
+# print("All found numbers \n", numbers)
 
 # Find all numbers that are close to symbols
 
+approved_numbers: list[int] = []
+
 for row_num, last_char_idx, value in numbers:
 
-    first_char_idx: int = last_char_idx - len(value) - 1
-    # check above
-    if row_num != 0:
-        if first_char_idx == 0:
-            for symbol_coordinate in symbol_indexes:
-                print(symbol_coordinate)
-                print("row_num-1: ", row_num-1)
-                print("first_char_idx-1: ", first_char_idx-1)
-                if symbol_coordinate[0] == row_num-1 and \
-                    symbol_coordinate[1] == first_char_idx-1:
-                    print("Found a symbol above???")
-                    print("symbol: ", symbol_coordinate, \
-                          "row_num, last_char_idx, value: ", row_num, last_char_idx, value)
+    first_char_idx: int = int(last_char_idx - len(value) + 1)
+    first_search: int = first_char_idx - 1
+    last_search: int =  last_char_idx + 1
+
+    # above
+    for coordinate in symbol_indexes:
+        if coordinate[0] == row_num-1:
+            if coordinate[1] >= first_search \
+            and coordinate[1] <= last_search:
+                approved_numbers.append(int(value))
+                # print(f"value {value} approved for above")
+
+    # same row
+    for coordinate in symbol_indexes:
+        if coordinate[0] == row_num:
+            if coordinate[1] == first_search or coordinate[1] == last_search:
+                approved_numbers.append(int(value))
+                # print(f"value {value} approved for same row")
+
+    # bellow
+    for coordinate in symbol_indexes:
+        if coordinate[0] == row_num+1:
+            if coordinate[1] >= first_search \
+            and coordinate[1] <= last_search:
+                approved_numbers.append(int(value))
+                # print(f"value {value} approved for bellow")
+
+# approved_numbers = approved_numbers)
+print(sum(approved_numbers))
+
 
 
 
